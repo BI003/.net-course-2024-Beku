225ce0aa47a2bd61ebc735ae5cddea44b9e3e739
Иван Беку <ivanbeku2003@mail.ru>
2024-09-20 19:30:34 +0300

    Создана папка “Domain”, в неё добавлена “Библиотека классов” - “BankSystem.Domain”, в неё добавлена папка “Models”. Добавлены классы: “Person” (человек),  “Employee” (сотрудник), “Client” (клиент), и структура “Currency” (валюта). В папку “Tools” добавлен проект (Консольное приложение) - “Practice”.

---
+++ BankSystem.Domain/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystem.Domain.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Passport { get; set; }
+        public int Number {  get; set; }
+    }
+}
